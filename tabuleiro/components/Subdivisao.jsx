8a70785a615266df9cd1b1d224eff37242f61047
eltoncelestino <eltongf2014@gmail.com>
2022-03-08 12:05:58 -0300

    Tabuleiro

---
+++ tabuleiro/components/Subdivisao.jsx
@@ -0,0 +1,14 @@
+import styles from '../styles/Subdivisao.module.css';
+
+export default function Subdivisao(props) {
+  return (
+    <div 
+      className={styles.subdivisao}
+      style={{
+        backgroundColor: props.preta ? "#000" : "#fff",
+      }}
+    >
+
+    </div>
+  )
+}
